d4532040eb0a87eae9954919cefeee92cc0de92f
earizmendi2 <65973618+earizmendi2@users.noreply.github.com>
2020-10-19 10:14:33 -0600

    [ADD] app print_journal_entires

---
+++ print_journal_entires/__manifest__.py
@@ -0,0 +1,28 @@
+
+
+
+{
+    'name': 'Print Journal Entries',
+    'version': '1.0',
+    'category': 'account',
+    'summary': 'Print Journal Entry',
+    'description': """
+    this module use for print journal Entries in PDF report"
+    """,
+    'author': "HAK Solutions",
+    'website': "http://haksolutions.com",
+    'depends': ['account'],
+    'license': 'AGPL-3',
+    'data': [
+            'report/report_menu.xml'
+            ],
+
+    'demo': [],
+    "images": [
+        'static/description/icon.png'
+    ],
+    'price': 00,
+    'currency': 'EUR',
+    'installable': True,
+    'auto_install': False,
+}
---
+++ print_journal_entires/journal_entries.py
@@ -0,0 +1,37 @@
+# -*- encoding: UTF-8 -*-
+##############################################################################
+#
+#    Odoo, Open Source Management Solution
+#    Copyright (C) 2015-Today Laxicon Solution.
+#    (<http://laxicon.in>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+##############################################################################
+
+# import models
+from odoo import api, models
+class accountdeit(models.Model):
+    _inherit = "account.move"
+
+    def total_debit_credit(self):
+        res = {}
+        for move in self:
+            dr_total = 0.0
+            cr_total = 0.0
+            for line in move.line_ids:
+                dr_total += line.debit
+                cr_total += line.credit
+            res.update({'cr_total': cr_total, 'dr_total': dr_total})
+        return res
