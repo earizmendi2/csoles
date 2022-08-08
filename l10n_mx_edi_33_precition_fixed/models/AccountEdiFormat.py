202767c97b08fbb6be2b47b50b3ed4e622be6796
LArizmendi96 <35753084+LArizmendi96@users.noreply.github.com>
2022-08-08 10:13:25 -0600

    Update AccountEdiFormat.py
    Hotfix

--- l10n_mx_edi_33_precition_fixed/models/AccountEdiFormat.py
+++ l10n_mx_edi_33_precition_fixed/models/AccountEdiFormat.py
@@ -1,6 +1,7 @@
+import logging
 from odoo import _, api, fields, models
 
-
+_logger = logging.getLogger(__name__)
 class AccountEdiFormat(models.Model):
     _inherit = 'account.edi.format'
 
@@ -16,7 +17,9 @@ class AccountEdiFormat(models.Model):
                 x['tax_amount_currency'], cfdi_values['currency_precision']) for x in details)
             total_tax_details_transferred += cfdi_values['balance_multiplicator'] * \
                 tax_detail_vals['tax_amount_currency']
-
+            
+        _logger.log(round(total_tax_details_transferred, cfdi_values['currency_precision']))
+        
         cfdi_values.update({
             'total_tax_details_transferred': round(total_tax_details_transferred, cfdi_values['currency_precision'])
         })
