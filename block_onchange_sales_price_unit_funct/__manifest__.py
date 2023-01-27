8914e410cd5699eebb8a313a434490cba1aafd08
ilegardacs <118316667+ilegardacs@users.noreply.github.com>
2023-01-27 19:37:15 +0000

    Se agrega bloqueo para no act onchange price unit

---
+++ block_onchange_sales_price_unit_funct/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Redondeo de Subtotal IL',
+    'version': '1.1',
+    'images':['static/description/icon.png'],
+    'summary': """Redonedo de lineas antes de calcular subtotal ileg y precios no actualizables ne lineas""",
+    'description': "",
+    'category': 'Generic Modules',
+    'author': 'Ivan Legarda',
+    'depends': [
+                'sale',
+    ],
+    'data': [],
+    
+    # 'demo': ['data/demo.xml'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'application': True,
+}
+
