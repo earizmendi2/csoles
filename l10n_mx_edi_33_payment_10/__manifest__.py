569a2dbbc6fbbc24dd2bbbec774bb391aaeb5246
ilegardacs <118316667+ilegardacs@users.noreply.github.com>
2023-02-03 05:59:43 +0000

    actualizacion desc de manifst payments

--- l10n_mx_edi_33_payment_10/__manifest__.py
+++ l10n_mx_edi_33_payment_10/__manifest__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'EDI Payment truncated 2 digits',
-    'version': '1.0',
+    'version': '1.2',
     'images':['static/description/icon.png'],
     'summary': 'REP TRUNCADO A 2 DIGITOS',
-    'description': "",
+    'description': "Toma los valores de los pagos y los trunca a 2 digitos para poderlos timbrar",
     'category': 'Generic Modules',
-    'author': 'McLennan Foster',
+    'author': 'Ivan Legarda',
     'depends': [
                 'base',                
                 'account_edi',
