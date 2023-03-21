2fdfd2357409d451a0b275cdae8db79aef80f2ea
ilegardacs <118316667+ilegardacs@users.noreply.github.com>
2023-03-21 19:45:26 +0000

    def balance check upd

---
+++ account_cancel_balance_check/__manifest__.py
@@ -0,0 +1,20 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Cancel Balance Check on Account Move",
+    "version": "15.0.1.2.1",
+    "category": "Accounting",
+    "license": "AGPL-3",
+    "summary": "Cancela la validación de balance debito credito, cuando marca error por diferencia de decimales",
+    "author": "Ivan Legarda",
+    "maintainers": ["Ivan Legarda"],
+    "website": "",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "",
+    ],
+    
+    "installable": True,
+}
--- account_cancel_balance_check/models/account_move.py
+++ account_cancel_balance_check/models/account_move.py
@@ -1,12 +1,15 @@
+# Copyright 2021 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from odoo import api, fields, models
+
 
 class AccountMove(models.Model):
     _inherit = "account.move"
 
     def _check_balanced(self):
-        #OVERRIDE FUNCTION
         ''' Assert the move is fully balanced debit = credit.
         An error is raised if it's not the case.
         '''
         return True
-        
