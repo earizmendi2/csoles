e3d881f250da4411228e012338fdfd8d209ba1fd
ilegardacs <118316667+ilegardacs@users.noreply.github.com>
2023-03-21 20:27:55 +0000

    def balanced check mod

---
+++ cancel_checke_balanced_mod/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2013-2020 Akretion France (https://akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Cancel Balance Check Validation",
+    "version": "15.0.1.0.3",
+    "category": "Invoices & Payments",
+    "license": "AGPL-3",
+    "summary": "Cancela la validación cuando se presenta una diferencia por cambio de decimales",
+    "author": "ILEG",
+    "website": "",
+    "depends": ["account"],
+    "data": [
+        "",
+    ],
+    "installable": True,
+}
