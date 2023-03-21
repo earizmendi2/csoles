89217c15852b398062468d9ccfdca55a918a741f
ilegardacs <118316667+ilegardacs@users.noreply.github.com>
2023-03-21 17:56:35 +0000

    def _check_balanced

---
+++ cancel_check_balanced/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Cancel account move check balanced',
+    'version': '1.0',
+    'images':['static/description/icon.png'],
+    'summary': 'Cancela la revision de balance en los documentos debito credito al crear un doc relacionado',
+    'description': "",
+    'category': 'Generic Modules',
+    'author': 'McLennan Foster',
+    'depends': [
+                'account_move',                
+    ],
+    'data': [
+        '',
+    ],        
+    'license': 'AGPL-3',
+    'installable': True,
+    'application': True,
+}
