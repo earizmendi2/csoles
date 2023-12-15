ce5443f41d6aebd487d9abea3d7392085a81ed20
ilegardacs <118316667+ilegardacs@users.noreply.github.com>
2023-12-15 21:20:32 +0000

    block onchange v16

---
+++ block_onchange_sales_priceunit_funct/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Bloqueo onchange PriceUnit ',
+    'version': '1.2',
+    'images':['static/description/icon.png'],
+    'summary': """Para que no se actualice el precio unitario al cambiar la cantidad de una sale order line""",
+    'description': "",
+    'category': 'Generic Modules',
+    'author': 'Ivan Legarda Dominguez',
+    'depends': [
+                'sale',
+    ],
+    'data': [],
+    
+    # 'demo': ['data/demo.xml'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'application': True,
+}
+
---
+++ block_onchange_sales_priceunit_funct/models/sale_order_line.py
@@ -0,0 +1,27 @@
+from odoo import models, fields, api
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+    
+    @api.depends('product_id', 'product_uom', 'product_uom_qty')
+    def _compute_price_unit(self):
+        for line in self:
+            # check if there is already invoiced amount. if so, the price shouldn't change as it might have been
+            # manually edited
+            if line.qty_invoiced > 0:
+                continue
+            if not line.product_uom or not line.product_id:
+                line.price_unit = 0.0
+            else:
+                price = line.with_company(line.company_id)._get_display_price()
+                if line.price_unit < 1:
+                    line.price_unit = line.product_id._get_tax_included_unit_price(
+                        line.company_id,
+                        line.order_id.currency_id,
+                        line.order_id.date_order,
+                        'sale',
+                        fiscal_position=line.order_id.fiscal_position_id,
+                        product_price_unit=price,
+                        product_currency=line.currency_id
+                    )
+            
