10d08d9d2394206a5b206c7d143ca5d88766c9e1
ilegardacs <118316667+ilegardacs@users.noreply.github.com>
2024-02-19 18:33:46 +0000

    quitar sat v4

--- rodo_sat_sync_mx/models/purchase_order.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields
-
-class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
-    
-    l10n_mx_edi_cfdi_uuid_cusom = fields.Char(string='Fiscal Folio UUID', copy=False, readonly=True)
