a220d72b552ccaf444690458ecbdf1921d8ecd88
ilegardacs <118316667+ilegardacs@users.noreply.github.com>
2025-02-18 16:03:05 -0600

    Update __manifest__.py

--- ms_report_stock/__manifest__.py
+++ ms_report_stock/__manifest__.py
@@ -9,7 +9,7 @@
     "author": "Miftahussalam",
     "website": "https://blog.miftahussalam.com/",
     "category": "Inventory/Inventory",
-    "version": "17.0.1",
+    "version": "17.1",
     "depends": [
         "base",
         "stock",
