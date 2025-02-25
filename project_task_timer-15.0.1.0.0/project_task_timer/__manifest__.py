38d88e8bd555b2597fee58a86cc5bc6b66821de6
ilegardacs <118316667+ilegardacs@users.noreply.github.com>
2025-02-25 10:14:14 -0600

    Update __manifest__.py

--- project_task_timer-15.0.1.0.0/project_task_timer/__manifest__.py
+++ project_task_timer-15.0.1.0.0/project_task_timer/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Project Task Timer',
-    'version': '15.0.1.0.0',
+    'version': '17.0',
     'summary': """Task Timer With Start & Stop""",
     'description': """"This module helps you to track time sheet in project automatically.""",
     'category': 'Project',
