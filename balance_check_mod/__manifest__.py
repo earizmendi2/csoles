005616e937dad73e921f02868204efa5392b55f5
RamonArcaLabs <ramon.reyna@arcalabs.com.mx>
2025-06-14 15:49:49 -0600

    add modules migrated 18 (#7)

--- balance_check_mod/__manifest__.py
+++ balance_check_mod/__manifest__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Cancelar revision de balance en Devoluciones ',
-    'version': '1.2',
     'images':['static/description/icon.png'],
     'summary': """Cancela la validación de debito y credito cuando las notas marcan un error de dif de 0.01""",
     'description': "",
--- product_pricelist_direct_print-13.0.1.0.1/product_pricelist_direct_print/__manifest__.py
+++ product_pricelist_direct_print-13.0.1.0.1/product_pricelist_direct_print/__manifest__.py
@@ -4,7 +4,6 @@
     "name": "Product Pricelist Direct Print",
     "summary": "Print price list from menu option, product templates, "
     "products variants or price lists",
-    "version": "13.0.1.0.1",
     "category": "Product",
     "website": "https://www.github.com/OCA/product-attribute",
     "author": "Tecnativa, " "Odoo Community Association (OCA)",
